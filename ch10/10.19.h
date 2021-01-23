262bce75488794df3d055e7589db1702b3aece0c
Aaron Hao (hanhao) <hanhao@cisco.com>
2021-01-23 23:16:46 +0800

    10.18

--- ch10/10.19.h
+++ ch10/10.19.h
@@ -1,10 +1,10 @@
-#ifndef CPP_PRIMER_ANSWERS_10_18_H
-#define CPP_PRIMER_ANSWERS_10_18_H
+#ifndef CPP_PRIMER_ANSWERS_10_19_H
+#define CPP_PRIMER_ANSWERS_10_19_H
 #include "../header.h"
 /*
- * desc : 重写biggies，用partition代替find_if。我们在10.3.1节练习10.13（第345页）中介绍了partition算法。
+ * desc :
+ * 用stable_partition重写前一题的程序，与stable_sort类似，在划分后的序列中维持原有元素的顺序。
  */
-
 class Demo {
 public:
     static void elimdups(vector<string> &vec)
@@ -17,7 +17,7 @@ public:
     static void biggies(vector<string> &vec, size_t sz)
     {
         elimdups(vec);
-        stable_sort(vec.begin(), vec.end(),
+        stable_partition(vec.begin(), vec.end(),
                     [](string const& l, string const& r) {return l.size() < r.size(); }
         );
 
@@ -31,8 +31,7 @@ public:
 
     static void test() {
         vector<string> v{"124", "123", "124", "13892", "25001", "213786", "asdhkhj"};
-        biggies(v, 4);
+        biggies(v, 3);
     }
 };
-
-#endif //CPP_PRIMER_ANSWERS_10_18_H
+#endif //CPP_PRIMER_ANSWERS_10_19_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "ch10/10.17.h"
+#include "ch10/10.18.h"
 
 int main() {
     Demo::test();
